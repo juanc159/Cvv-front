86bc6a456bd9553bb25f907de75bdfa6f7857ab2
Juan Carlos Moreno Guerra <juan.moreno@designingsolutions.com.co>
2025-01-11 17:17:37 -0500

    cambios funcionales

--- dist/assets/VSpacer-DpDHzE9j.js
+++
@@ -1,1 +0,0 @@
-import"./VGrid-D5nqm1T5.js";import{c as r}from"./VAvatar-Dqwm1zz1.js";const e=r("v-spacer","div","VSpacer");export{e as V};
---
+++ dist/assets/VSpacer-tm-ml0yH.js
@@ -0,0 +1,1 @@
+import"./VGrid-Bt8CnWYw.js";import{c as r}from"./VAvatar-Cu-AfI-t.js";const e=r("v-spacer","div","VSpacer");export{e as V};
