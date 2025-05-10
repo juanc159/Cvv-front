db76e28f3781e75e282fbf98c8956070eadf43de
Juan Carlos Moreno Guerra <juan.moreno@designingsolutions.com.co>
2025-05-10 16:15:02 -0500

    cambios nuevos

--- dist/assets/VCardText-BdD3zWfb.js
+++
@@ -1,1 +0,0 @@
-import{ag as s,ah as r,ai as o,b as c,aj as n,ak as m}from"./index-BQ2WxUSv.js";const p=r({opacity:[Number,String],...m(),...n()},"VCardText"),i=s()({name:"VCardText",props:p(),setup(a,e){let{slots:t}=e;return o(()=>c(a.tag,{class:["v-card-text",a.class],style:[{"--v-card-text-opacity":a.opacity},a.style]},t)),{}}});export{i as V};
