7cfe68522892e7dbec4501bc4e834a5c2670d7cc
Juan Carlos Moreno Guerra <juan.moreno@designingsolutions.com.co>
2025-05-10 16:19:23 -0500

    Cambios funcionales

--- dist/assets/Index-C8B0xbHi.js
+++
@@ -1,1 +0,0 @@
-import{d as e,c as n,o}from"./index-CgnQDxxm.js";const s=e({__name:"Index",setup(t){return(a,c)=>(o(),n("div"))}});export{s as default};
--- dist/assets/ssrBoot-8rNYjAxW.js
+++
@@ -1,1 +0,0 @@
-import{az as t,M as e,X as s,bO as n}from"./index-CgnQDxxm.js";function i(){const o=t(!1);return e(()=>{window.requestAnimationFrame(()=>{o.value=!0})}),{ssrBootStyles:s(()=>o.value?void 0:{transition:"none !important"}),isBooted:n(o)}}export{i as u};
