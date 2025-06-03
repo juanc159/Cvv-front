8ce1c84d7c03e5a2af353d70f5eb175ec340e9f4
Juan Carlos Moreno Guerra <juan.moreno@designingsolutions.com.co>
2025-06-03 17:14:27 -0500

    Cambios funcionales

--- dist/assets/VSpacer-C69qXvdc.js
+++
@@ -1,1 +0,0 @@
-/* empty css              */import{c as r}from"./VAvatar-wfGGIPDq.js";const e=r("v-spacer","div","VSpacer");export{e as V};
--- dist/assets/ssrBoot-CZW1b9Lq.js
+++
@@ -1,1 +0,0 @@
-import{aA as t,M as e,X as s,bJ as n}from"./index-BJUCe1vL.js";function i(){const o=t(!1);return e(()=>{window.requestAnimationFrame(()=>{o.value=!0})}),{ssrBootStyles:s(()=>o.value?void 0:{transition:"none !important"}),isBooted:n(o)}}export{i as u};
