18f854df169e65c7f0316be07af8f14cea5e0c0f
Juan Carlos Moreno Guerra <jcmg.ing@gmail.com>
2025-07-27 19:26:37 -0500

    Cambios Funcionales

---
+++ dist/assets/ssrBoot-hKX_sz8Y.js
@@ -0,0 +1,1 @@
+import{ah as t,N as e,Y as s,bT as n}from"./index-BjO4aHmb.js";function i(){const o=t(!1);return e(()=>{window.requestAnimationFrame(()=>{o.value=!0})}),{ssrBootStyles:s(()=>o.value?void 0:{transition:"none !important"}),isBooted:n(o)}}export{i as u};
