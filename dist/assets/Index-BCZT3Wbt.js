25231256b8bca4fbe4e0b49de753070bcff77349
Juan Carlos Moreno Guerra <jcmg.ing@gmail.com>
2025-07-27 23:20:54 -0500

    Cambios Funcionales

---
+++ dist/assets/Index-BCZT3Wbt.js
@@ -0,0 +1,1 @@
+import{d as e,c as n,o}from"./index-v0Pi_bgb.js";const s=e({__name:"Index",setup(t){return(a,c)=>(o(),n("div"))}});export{s as default};
---
+++ dist/assets/ssrBoot-w07Os5ZS.js
@@ -0,0 +1,1 @@
+import{ah as t,N as e,Y as s,bT as n}from"./index-v0Pi_bgb.js";function i(){const o=t(!1);return e(()=>{window.requestAnimationFrame(()=>{o.value=!0})}),{ssrBootStyles:s(()=>o.value?void 0:{transition:"none !important"}),isBooted:n(o)}}export{i as u};
