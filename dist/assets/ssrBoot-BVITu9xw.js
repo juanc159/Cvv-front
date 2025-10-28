cc77cea2cfa088a1b88f62667ca117f96c527e87
Juan Carlos Moreno Guerra <jcmg.ing@gmail.com>
2025-10-28 10:01:13 -0500

    c

--- dist/assets/ssrBoot-BVITu9xw.js
+++
@@ -1,1 +0,0 @@
-import{am as t,N as e,Y as s,bV as n}from"./index-CPwhtSgK.js";function i(){const o=t(!1);return e(()=>{window.requestAnimationFrame(()=>{o.value=!0})}),{ssrBootStyles:s(()=>o.value?void 0:{transition:"none !important"}),isBooted:n(o)}}export{i as u};
