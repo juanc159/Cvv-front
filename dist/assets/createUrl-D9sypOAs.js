610e29a5c53ee2b4705428d8efae70db5ab85c6e
juancthecloud <jcmg.ing@gmail.com>
2026-01-19 18:08:22 -0500

    cambios

--- dist/assets/createUrl-D9sypOAs.js
+++
@@ -1,1 +0,0 @@
-import{s as f}from"./index-0F-JFElI.js";import{Y as q,cI as e}from"./index-CtOr4Wu7.js";const s=(c,r)=>q(()=>{if(!(r!=null&&r.query))return e(c);const t=e(c),a=e(r==null?void 0:r.query),u=Object.fromEntries(Object.entries(a).map(([m,y])=>[m,e(y)]));return`${t}${u?`?${f(u)}`:""}`});export{s as c};
